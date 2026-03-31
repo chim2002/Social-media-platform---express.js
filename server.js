8ffcb2f3cb6f1ef3b2168977f1919b61cc83c7e5
Induni Warnachinthaka <it23310278@my.sliit.lk>
2026-03-31 15:22:18 +0530

    Add CRUD API for posts

--- server.js
+++ server.js
@@ -3,10 +3,54 @@ const app = express();
 
 const PORT = 3000;
 
-// simple route
-app.get('/', (req, res) => {
-    res.send('Server is running 🚀');
+// middleware
+app.use(express.json());
+
+// in-memory data store
+let posts = [];
+app.post('/posts', (req, res) => {
+    const { content } = req.body;
+
+    const newPost = {
+        id: posts.length + 1,
+        content: content
+    };
+
+    posts.push(newPost);
+
+    res.status(201).json(newPost);
 });
+
+//get all posts
+app.get('/posts', (req, res) => {
+    res.json(posts);
+});
+
+// update a post
+app.put('/posts/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const { content } = req.body;
+
+    const post = posts.find(p => p.id === id);
+
+    if (!post) {
+        return res.status(404).send('Post not found');
+    }
+
+    post.content = content;
+
+    res.json(post);
+});
+
+// delete a post
+app.delete('/posts/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+
+    posts = posts.filter(p => p.id !== id);
+
+    res.send('Post deleted');
+});
+
 
 // start server
 app.listen(PORT, () => {
