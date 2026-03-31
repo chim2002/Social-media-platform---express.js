5503a9552fa31904cba207767e5402c101c3cdb3
Induni Warnachinthaka <it23310278@my.sliit.lk>
2026-03-31 21:24:28 +0530

    Add JWT authentication

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ app.use(express.json());
 
 // in-memory data store
 let posts = [];
-app.post('/posts', (req, res) => {
+
+app.post('/posts', authenticateToken, (req, res) => {
     const { content } = req.body;
 
     const newPost = {
@@ -27,7 +28,7 @@ app.get('/posts', (req, res) => {
 });
 
 // update a post
-app.put('/posts/:id', (req, res) => {
+app.put('/posts/:id', authenticateToken, (req, res) => {
     const id = parseInt(req.params.id);
     const { content } = req.body;
 
@@ -43,7 +44,7 @@ app.put('/posts/:id', (req, res) => {
 });
 
 // delete a post
-app.delete('/posts/:id', (req, res) => {
+app.delete('/posts/:id', authenticateToken, (req, res) => {
     const id = parseInt(req.params.id);
 
     posts = posts.filter(p => p.id !== id);
@@ -56,3 +57,47 @@ app.delete('/posts/:id', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
+
+//add fake user
+const jwt = require('jsonwebtoken');
+
+const SECRET_KEY = "mysecretkey";
+
+// fake user
+const user = {
+    id: 1,
+    username: "admin",
+    password: "1234"
+};
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    if (username === user.username && password === user.password) {
+        const token = jwt.sign(
+            { id: user.id, username: user.username },
+            SECRET_KEY,
+            { expiresIn: '1h' }
+        );
+
+        return res.json({ token });
+    }
+
+    res.status(401).send('Invalid credentials');
+});
+
+//auth middleware
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) return res.sendStatus(401);
+
+    jwt.verify(token, SECRET_KEY, (err, userData) => {
+        if (err) return res.sendStatus(403);
+
+        req.user = userData;
+        next();
+    });
+}
